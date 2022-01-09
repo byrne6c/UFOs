2b166644042d8e762790bfcb348771ad738bf013
byrne6c <91990957+byrne6c@users.noreply.github.com>
2022-01-09 06:42:17 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 // import the data from data.js
 const tableData = data;
 // Reference the HTML table using d3
-var tbody = d3.select("tbody");
-// start a new function to build table
-function buildTable(data) {
-    //clear any existing data
-	tbody.html("");
-    // loop through each object in the data
-    data.forEach((dataRow) => {
-    // add arguement: append row & cells for each value in the row
-        let row = tbody.append("tr");
-    // set a for loop function for each datafield within array, for the datarow within the table    
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-    // append each value as a table cell (td)   
-            cell.text(val);
-        }
-    );
+// Get a reference to the table using t3
+let tbody = d3.select("tbody");
+// Console.log the weather data from data.js
+console.log(data);
+data.forEach(function(data) {
+ console.log(data);
+ let row = tbody.append("tr");
+ Object.entries(data).forEach(function([key, value]) {
+   console.log(key, value);
+   // Append a cell to the row for each value
+   // in the weather report object
+   let cell = row.append("td");
+   cell.text(value);
+ });
 });
 
 // start new function to filter data by date 
