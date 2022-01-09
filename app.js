a7a2ed097f66ce959f1cd2c18e2fc7e265675d78
byrne6c <91990957+byrne6c@users.noreply.github.com>
2022-01-09 18:57:45 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 // import the data from data.js
 const tableData = data;
+console.log("hello wolrd!");
 // Reference the HTML table using d3
 // Get a reference to the table using t3
 let tbody = d3.select("tbody");
-// Console.log the weather data from data.js
 console.log(data);
-data.forEach(function(data) {
- console.log(data);
- let row = tbody.append("tr");
- Object.entries(data).forEach(function([key, value]) {
-   console.log(key, value);
-   // Append a cell to the row for each value
-   // in the weather report object
-   let cell = row.append("td");
-   cell.text(value);
- });
-});
-
-// start new function to filter data by date 
+function buildTable(data) {
+data.forEach(function(dataRow) {
+    console.log(dataRow);
+    let row = tbody.append("tr");
+    Object.entries(dataRow).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+} // start new function to filter data by date 
 function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
     
     // Check to see if a date was entered and filter the data using that date.
-   if (date) {
+    if (date) {
     // Apply `filter` to the table data to only keep the
     // rows where the `datetime` value matches the filter value
         filteredData = filteredData.filter(row => row.datetime === date);
@@ -40,4 +40,3 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
-}
